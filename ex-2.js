2eefa1a1c755bed9c07155cf766b82ad24d54da1
noppadon <noppadonsangthongw@gmail.com>
2023-12-20 18:24:56 +0700

    edit new

--- ex-2.js
+++ ex-2.js
@@ -2,19 +2,13 @@
 
 const isPalindrome = function (s) {
   //Start Coding here
-  reversString = s.toLowerCase().replace(/[^a-zA-Z]/g, '').reverse();
-  console.log(newS)
-  let reversString = "";
-  for(i=newS.length-1; i>=0; i--){
-    reversString = reversString+newS[i]
-  }
-  return reversString === newS ? true : false
+  return s.replace(/[^a-zA-Z]/g, '').toLowerCase().split("").reverse().join("") === s.replace(/[^a-zA-Z]/g, '').toLowerCase() ? true : false; 
 };
 
 const result1 = isPalindrome("A man, a plan, a canal: Panama");
 const result2 = isPalindrome("race a car");
 const result3 = isPalindrome(" ");
 
-console.log(result1); // true 
+console.log(result1); // true
 console.log(result2); // false
 console.log(result3); // true
